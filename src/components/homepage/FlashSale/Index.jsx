75b8b8e3bd3731a3bf76b1c4f7ea0562066c0bd4
sharifstack <sharifahmed.sh408@gmail.com>
2025-10-24 20:23:24 +0600

    slider responsive done

--- src/components/homepage/FlashSale/Index.jsx
+++ src/components/homepage/FlashSale/Index.jsx
@@ -20,8 +20,8 @@ const FlashSale = () => {
           Loading={isLoading}
         />
 
-        <div className="allProductsbtn  flex justify-center border-b border-HoverButton2_A0BCE0 pb-[60px]">
-          <h2 className="bg-Button2_DB4444 font-poppins text-base font-medium py-4 px-12 text-text_FAFAFA cursor-pointer rounded">
+        <div className="allProductsbtn  flex justify-center border-b border-HoverButton2_A0BCE0 pb-6 sm:pb-[60px]">
+          <h2 className="bg-Button2_DB4444 font-poppins text-sm sm:text-base font-medium py-4 px-6 sm:px-12 text-text_FAFAFA cursor-pointer rounded">
             View All Products
           </h2>
         </div>
