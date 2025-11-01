9c8c50267efb2f665b0b72064b7ce395e14682a2
sharifstack <sharifahmed.sh408@gmail.com>
2025-11-01 17:00:56 +0600

    sidebar fixed

--- src/components/RootLayOut/Navbar/Index.jsx
+++ src/components/RootLayOut/Navbar/Index.jsx
@@ -189,13 +189,13 @@ const Navbar = () => {
 
                 <div
                   id="drawer-navigation"
-                  class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-64 dark:bg-gray-800"
+                  class="fixed top-0 left-0 z-50 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-64 dark:bg-Text1_7D8184 "
                   tabindex="-1"
                   aria-labelledby="drawer-navigation-label"
                 >
                   <h5
                     id="drawer-navigation-label"
-                    class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400"
+                    class="text-base font-semibold text-white uppercase "
                   >
                     Menu
                   </h5>
@@ -206,7 +206,7 @@ const Navbar = () => {
                     class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
                   >
                     <svg
-                      class="w-3 h-3"
+                      class="w-3 h-3 text-white"
                       aria-hidden="true"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
@@ -222,15 +222,15 @@ const Navbar = () => {
                     </svg>
                     <span class="sr-only">Close menu</span>
                   </button>
-                  <div class="py-4 overflow-y-auto">
+                  <div class="py-4 overflow-y-auto ">
                     <ul class="space-y-2 font-medium">
                       <li>
                         <a
                           href="#"
-                          class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                          class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                         >
                           <svg
-                            class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                            class="w-5 h-5 text-white transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white"
                             aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg"
                             fill="currentColor"
