f91c5261ae3d901b2a80e01a834c5133c021a9d6
sharifstack <sharifahmed.sh408@gmail.com>
2026-05-02 17:35:07 +0600

    Feat: Chekout

--- src/components/CartComponents/Index.jsx
+++ src/components/CartComponents/Index.jsx
@@ -3,9 +3,18 @@ import BreadCrumb from "../CommonComponents/BreadCrumb";
 import cart from "../../assets/cart/cart.png";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 import useCart from "../../helpers/hooks/useCart";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const CartComponents = () => {
-  const { cartItems, incrementCartItem, decrementCartItem } = useCart();
+  const { cartItems, incrementCartItem, decrementCartItem, removeCartItem } =
+    useCart();
+
+  const handleRemove = (item) => {
+    removeCartItem(item.id, item.selectedSize, item.selectedColor);
+
+    toast.success(`${item.title} removed from cart`);
+  };
 
   const total = cartItems.reduce(
     (sum, item) => sum + item.price * item.quantity,
@@ -13,99 +22,109 @@ const CartComponents = () => {
   );
   return (
     <div className="mb-[140px]">
-      <div className="container">
+      <div className="container ">
         <BreadCrumb />
 
-        <div className="flex flex-col gap-10">
-          <div className="flex border shadow-lg">
-            <div className=" w-3/12 py-6 px-10">
-              <h2 className="font-poppins font-normal text-base text-Text2_000000">
-                Product
-              </h2>
-            </div>
-            <div className=" w-3/12 py-6 px-10">
-              <h2 className="font-poppins font-normal text-base text-Text2_000000 text-center">
-                Price
-              </h2>
-            </div>
-            <div className="w-3/12 py-6 px-10">
-              <h2 className="font-poppins font-normal text-base text-Text2_000000 text-center">
-                Quantity
-              </h2>
-            </div>
-            <div className=" w-3/12  py-6 px-10">
-              <h2 className="font-poppins font-normal text-base text-Text2_000000 text-end">
-                Subtotal
-              </h2>
-            </div>
-          </div>
-          {cartItems.map((item) => (
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-5 w-8/12">
             <div className="flex border shadow-lg">
-              <div className="flex items-center gap-5 w-3/12 py-6 px-10">
-                <div>
-                  <img src={item.image} alt={item.name} />
-                </div>
+              <div className=" w-3/12 py-6 px-10">
                 <h2 className="font-poppins font-normal text-base text-Text2_000000">
-                  {item.title}
+                  Product
                 </h2>
               </div>
-              <div className="flex items-center justify-center w-3/12 py-6 px-10">
-                <h2 className="font-poppins font-normal text-base text-Text2_000000">
-                  ${item.price}
+              <div className=" w-3/12 py-6 px-10">
+                <h2 className="font-poppins font-normal text-base text-Text2_000000 text-center">
+                  Price
                 </h2>
               </div>
-              <div className="w-3/12 py-6 px-10 flex justify-center">
-                <div className="flex items-center border rounded">
-                  <button
-                    onClick={() =>
-                      decrementCartItem(
-                        item.id,
-                        item.selectedSize,
-                        item.selectedColor,
-                      )
-                    }
-                    className="px-2"
-                  >
-                    -
-                  </button>
-
-                  <span className="px-3">{item.quantity}</span>
-
-                  <button
-                    onClick={() =>
-                      incrementCartItem(
-                        item.id,
-                        item.selectedSize,
-                        item.selectedColor,
-                      )
-                    }
-                    className="px-2"
-                  >
-                    +
-                  </button>
-                </div>
+              <div className="w-3/12 py-6 px-10">
+                <h2 className="font-poppins font-normal text-base text-Text2_000000 text-center">
+                  Quantity
+                </h2>
               </div>
-              <div className="flex items-center justify-end w-3/12  py-6 px-[60px]">
-                <h2 className="font-poppins font-normal text-base text-Text2_000000 ">
-                  ${(item.price * item.quantity).toFixed(2)}
+              <div className=" w-3/12  py-6 px-20">
+                <h2 className="font-poppins font-normal text-base text-Text2_000000 text-end">
+                  Subtotal
                 </h2>
               </div>
             </div>
-          ))}
+            {cartItems.map((item) => (
+              <div className="flex border shadow-lg">
+                <div className="flex items-center gap-6 w-3/12 py-6 px-10">
+                  <div>
+                    <img className="w-25" src={item.image} alt={item.name} />
+                  </div>
+                  <h2 className="font-poppins font-normal text-base text-Text2_000000 ">
+                    {item.title}
+                  </h2>
+                </div>
+                <div className="flex items-center justify-center w-3/12 py-6 px-10">
+                  <h2 className="font-poppins font-normal text-base text-Text2_000000">
+                    ${item.price}
+                  </h2>
+                </div>
+                <div className="w-3/12 py-6 px-10 flex items-center justify-center">
+                  <div className="flex items-center border rounded">
+                    <button
+                      onClick={() =>
+                        decrementCartItem(
+                          item.id,
+                          item.selectedSize,
+                          item.selectedColor,
+                        )
+                      }
+                      className="px-2 py-2"
+                    >
+                      -
+                    </button>
 
-          <div className="Buttons flex justify-between">
-            <button className="font-poppins text-base font-medium py-4 px-12  cursor-pointer rounded text-Text2_000000 bg-transparent border border-[rgba(0,0,0,0.5)] hover:bg-Button2_DB4444 hover:text-text_FAFAFA transition-all duration-100">
-              Return To Shop
-            </button>
+                    <span className="px-3 select-none">{item.quantity}</span>
 
-            <button className="font-poppins text-base font-medium py-4 px-12  cursor-pointer rounded text-Text2_000000 bg-transparent border border-[rgba(0,0,0,0.5)] hover:bg-Button2_DB4444 hover:text-text_FAFAFA transition-all duration-100">
-              Update Cart
-            </button>
+                    <button
+                      onClick={() =>
+                        incrementCartItem(
+                          item.id,
+                          item.selectedSize,
+                          item.selectedColor,
+                        )
+                      }
+                      className="px-2"
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+                <div className="flex items-center justify-end w-3/12  py-6 px-8">
+                  <h2 className="font-poppins font-normal text-base text-Text2_000000 ">
+                    ${(item.price * item.quantity).toFixed(2)}
+                  </h2>
+                </div>
+
+                <button
+                  onClick={() => handleRemove(item)}
+                  className="text-red-500 text-lg font-bold cursor-pointer pr-10"
+                >
+                  ✕
+                </button>
+              </div>
+            ))}
+
+            <div className="Buttons flex justify-between">
+              <Link to={"/"}>
+                <button className="font-poppins text-base font-medium py-4 px-12  cursor-pointer rounded text-Text2_000000 bg-transparent border border-[rgba(0,0,0,0.5)] hover:bg-Button2_DB4444 hover:text-text_FAFAFA transition-all duration-100">
+                  Return To Shop
+                </button>
+              </Link>
+
+              <button className="font-poppins text-base font-medium py-4 px-12  cursor-pointer rounded text-Text2_000000 bg-transparent border border-[rgba(0,0,0,0.5)] hover:bg-Button2_DB4444 hover:text-text_FAFAFA transition-all duration-100">
+                Remove all
+              </button>
+            </div>
           </div>
-        </div>
 
-        <div className="flex justify-between mt-20 ">
-          <div className="flex gap-4">
+          <div className=" ">
+            {/* <div className="flex gap-4">
             <div>
               <input
                 className="w-[300px] border border-Text2_000000 text-base font-poppins rounded text-Text2_000000 py-4 pl-6 pr-40"
@@ -119,43 +138,44 @@ const CartComponents = () => {
                 Apply Coupon
               </button>
             </div>
-          </div>
+          </div> */}
 
-          <div className="TotalCalculate  border-2 border-Text2_000000 rounded w-2/5 py-8 px-6">
-            <div>
-              <h4 className="font-poppins font-medium text-xl text-Text2_000000  pb-6">
-                Cart Total
-              </h4>
-              <div className="flex justify-between border-b pb-4">
-                <h5 className="font-poppins font-normal text-base text-Text2_000000">
-                  Subtotal:
-                </h5>
-                <h5 className="font-poppins font-normal text-base text-Text2_000000">
-                  ${total.toFixed(2)}
-                </h5>
-              </div>
+            <div className="TotalCalculate  border-2 border-Text2_000000 rounded w-[500px]  py-8 px-6">
+              <div>
+                <h4 className="font-poppins font-medium text-xl text-Text2_000000  pb-6">
+                  Cart Total
+                </h4>
+                <div className="flex justify-between border-b pb-4">
+                  <h5 className="font-poppins font-normal text-base text-Text2_000000">
+                    Subtotal:
+                  </h5>
+                  <h5 className="font-poppins font-normal text-base text-Text2_000000">
+                    ${total.toFixed(2)}
+                  </h5>
+                </div>
 
-              <div className="flex justify-between border-b py-4">
-                <h5 className="font-poppins font-normal text-base text-Text2_000000">
-                  Shipping:
-                </h5>
-                <h5 className="font-poppins font-normal text-base text-Text2_000000">
-                  Free
-                </h5>
-              </div>
+                <div className="flex justify-between border-b py-4">
+                  <h5 className="font-poppins font-normal text-base text-Text2_000000">
+                    Shipping:
+                  </h5>
+                  <h5 className="font-poppins font-normal text-base text-Text2_000000">
+                    Free
+                  </h5>
+                </div>
 
-              <div className="flex justify-between border-b py-4 mb-4">
-                <h5 className="font-poppins font-normal text-base text-Text2_000000">
-                  Total:
-                </h5>
-                <h5 className="font-poppins font-normal text-base text-Text2_000000">
-                  ${total.toFixed(2)}
-                </h5>
-              </div>
-              <div className="text-center">
-                <button className="font-poppins text-base font-medium py-4 px-12  cursor-pointer rounded bg-Button2_DB4444 text-text_FAFAFA ">
-                  Procees to checkout
-                </button>
+                <div className="flex justify-between border-b py-4 mb-4">
+                  <h5 className="font-poppins font-normal text-base text-Text2_000000">
+                    Total:
+                  </h5>
+                  <h5 className="font-poppins font-normal text-base text-Text2_000000">
+                    ${total.toFixed(2)}
+                  </h5>
+                </div>
+                <div className="text-center">
+                  <button className="font-poppins text-base font-medium py-4 px-12  cursor-pointer rounded bg-Button2_DB4444 text-text_FAFAFA ">
+                    Procees to checkout
+                  </button>
+                </div>
               </div>
             </div>
           </div>
